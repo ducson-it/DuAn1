bdfe93abbca6fe9e21bda34aa43c8dcb86de6d8d
ducsonprime <ducson.nuce@gmail.com>
2022-07-12 08:14:33 +0700

    readd file Sql

---
+++ duan1.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2022 at 03:09 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `duan1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+-- Error reading structure for table duan1.booking: #1932 - Table 'duan1.booking' doesn't exist in engine
+-- Error reading data for table duan1.booking: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `duan1`.`booking`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor_user`
+--
+-- Error reading structure for table duan1.doctor_user: #1932 - Table 'duan1.doctor_user' doesn't exist in engine
+-- Error reading data for table duan1.doctor_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `duan1`.`doctor_user`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gender`
+--
+-- Error reading structure for table duan1.gender: #1932 - Table 'duan1.gender' doesn't exist in engine
+-- Error reading data for table duan1.gender: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `duan1`.`gender`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `level`
+--
+-- Error reading structure for table duan1.level: #1932 - Table 'duan1.level' doesn't exist in engine
+-- Error reading data for table duan1.level: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `duan1`.`level`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient_users`
+--
+-- Error reading structure for table duan1.patient_users: #1932 - Table 'duan1.patient_users' doesn't exist in engine
+-- Error reading data for table duan1.patient_users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `duan1`.`patient_users`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+-- Error reading structure for table duan1.roles: #1932 - Table 'duan1.roles' doesn't exist in engine
+-- Error reading data for table duan1.roles: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `duan1`.`roles`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shifts`
+--
+-- Error reading structure for table duan1.shifts: #1932 - Table 'duan1.shifts' doesn't exist in engine
+-- Error reading data for table duan1.shifts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `duan1`.`shifts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table duan1.users: #1932 - Table 'duan1.users' doesn't exist in engine
+-- Error reading data for table duan1.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `duan1`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
